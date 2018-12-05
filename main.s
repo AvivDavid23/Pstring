dcdf37b790de7f8002c45d7d3430afc4bc9cd2d3
Aviv David <40210928+AvivDavid23@users.noreply.github.com>
2018-12-05 11:41:21 +0200

    Update main.s

--- main.s
+++ main.s
@@ -14,48 +14,48 @@ main:
 	movq	%rsp, %rsi
 	movq	$d, %rdi
 	movq	$0, %rax
-	call 	scanf			# scan first int
+	call 	scanf		# scan first int
 	movslq	(%rsp), %rsi	# get number
 	movb	(%rsp), %r12b	# first byte of length
-	dec		%rsp			# for \0
-	movb	$0, (%rsp)		# put \0
+	dec	%rsp		# for \0
+	movb	$0, (%rsp)	# put \0
 	subq	%rsi, %rsp	# add n bytes
 	movq	$s, %rdi
 	movq	%rsp, %rsi
 	movq	$0, %rax
-	call	scanf			# scan first string
-	dec		%rsp
+	call	scanf		# scan first string
+	dec	%rsp
 	movb	%r12b, (%rsp)	# put length
-	movq	%rsp, %r12		# points to first pstring
+	movq	%rsp, %r12	# points to first pstring
 
 	subq	$4, %rsp
 	movq	%rsp, %rsi
 	movq	$d, %rdi
 	movq	$0, %rax
-	call 	scanf			# scan second int
+	call 	scanf		# scan second int
 	movslq	(%rsp), %rsi	# get number
 	movb	(%rsp), %r13b	# first byte of length
-	dec		%rsp			# for \0
-	movb	$0, (%rsp)		# put \0
+	dec	%rsp		# for \0
+	movb	$0, (%rsp)	# put \0
 	subq	%rsi, %rsp	# add n bytes
 	movq	$s, %rdi
 	movq	%rsp, %rsi
 	movq	$0, %rax
-	call	scanf			# scan second string
-	dec		%rsp
+	call	scanf		# scan second string
+	dec	%rsp
 	movb	%r13b, (%rsp)	# put length
-	movq	%rsp, %r13		# points to second pstring
+	movq	%rsp, %r13	# points to second pstring
 
 	subq	$4, %rsp
 	movq	%rsp, %rsi
 	movq	$d, %rdi
 	movq	$0, %rax
-	call 	scanf			# scan choice
+	call 	scanf		# scan choice
 	movl 	(%rsp), %edi
 	movq	%r12, %rsi
 	movq	%r13, %rdx
 	call	run_func
-	#	now restore r12 and r13
+	# now restore r12 and r13
 	movzbq	(%r13), %rdx
 	addq	$2, %rsp
 	addq	%rdx, %rsp
