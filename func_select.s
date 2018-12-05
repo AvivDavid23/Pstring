f623e17218e07f140862d076e3283bb61019db02
Aviv David <40210928+AvivDavid23@users.noreply.github.com>
2018-12-05 11:34:25 +0200

    Add files via upload

---
+++ func_select.s
@@ -0,0 +1,202 @@
+	.section .rodata
+	input_err:  	.string "invalid option!\n" # for wrong input
+	func_50:		.string "first pstring length: %d, second pstring length: %d\n"
+	func_51:		.string "old char: %c, new char: %c, first string: %s, second string: %s\n"
+	f51_sc:     	.string "%c %c"
+	dummy:			.string	"%c"
+	func_52:		.string "length: %d, string: %s\n"
+	f52_sc:			.string "%d"
+	func_53:		.string "length: %d, string: %s\n"
+	func_54:		.string "compare result: %d\n"
+	f54_sc:			.string "%d"
+
+	.align 8
+   .SWITCH:		# jump table
+   	.quad .L50
+	.quad .L51
+	.quad .L52
+	.quad .L53
+	.quad .L54
+	.text 
+.globl	run_func
+run_func:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	leal	-50(%edi), %ecx
+	cmpl 	$4, %ecx
+	ja .ERR
+	jmp *.SWITCH(,%ecx,8)
+	.L50:
+	  pushq		%r12
+	  pushq		%r13
+	  pushq		%rdx
+	  movq		%rsi, %rdi 
+	  call  	pstrln 
+	  movzbl	%al, %r12d	# first length
+	  popq	    %rdi
+	  call 		pstrln 
+	  movzbl    %al, %r13d	# second length
+	  movq		$func_50, %rdi
+	  movl		%r12d, %esi
+	  movl		%r13d, %edx
+	  movq		$0, %rax
+	  call 		printf
+	  popq		%r13
+	  popq		%r12
+	  jmp .DONE
+	 
+	.L51:
+		pushq	%r12
+		pushq	%r13
+		movq	%rsi, %r12
+		movq	%rdx, %r13
+		subq	$1, %rsp
+		leaq	(%rsp), %rsi
+		movq	$dummy, %rdi
+		movq	$0, %rax
+		call	scanf		# first scan new lone from previous scan
+
+		subq	$2, %rsp
+		leaq	1(%rsp), %rsi
+		leaq	(%rsp), %rdx
+		movq	$f51_sc, %rdi
+		movq	$0, %rax
+		call	scanf		# scan 2 chars
+
+		movq	%r12, %rdi
+		movb	1(%rsp), %sil
+		movb	(%rsp), %dl
+		call 	replaceChar
+		movq	%rax, %r12
+
+		movq	%r13, %rdi
+		movb	1(%rsp), %sil
+		movb	(%rsp), %dl
+		call 	replaceChar
+		movq	%rax, %r13
+
+		movb	1(%rsp), %sil
+		movb	(%rsp), %dl
+		movq	%r12, %rcx
+		inc		%rcx	# skip length byte
+		movq	%r13, %r8
+		inc		%r8		# skip length byte
+		movq	$0, %rax
+		movq	$func_51, %rdi
+		call	printf
+		addq	$3, %rsp
+		popq	%r13
+		popq	%r12
+	  jmp .DONE
+	 
+	.L52:
+		pushq	%rsi	# save first ptr
+		pushq	%rdx	# save second ptr
+	  	subq	$8, %rsp
+		leaq	4(%rsp), %rsi
+		movq	$f52_sc, %rdi
+		movq	$0, %rax
+		call 	scanf	# scan first int
+		leaq	(%rsp), %rsi
+		movq	$f52_sc, %rdi
+		movq	$0, %rax
+		call 	scanf	# scan second int
+		movb	4(%rsp), %dl
+		movb	(%rsp), %cl
+		movq	8(%rsp), %rsi	# second ptr
+		movq	16(%rsp),%rdi	# first ptr
+		call 	pstrijcpy
+		movq	%rax, %rdx
+		inc		%rdx	# skip length byte
+		movzbl	(%rax), %esi	# get length
+		movq	$func_52, %rdi
+		movq	$0, %rax
+		call	printf
+		movq	8(%rsp), %rax	# rax will have second str ptr
+		movzbl	(%rax), %esi	# get length
+		movq	%rax, %rdx
+		inc		%rdx	# skip length byte
+		movq	$func_52, %rdi
+		movq	$0, %rax
+		call	printf
+		jmp .DONE			
+	 
+	.L53:
+		pushq	%r12
+		pushq	%r13
+		pushq	%r14
+		pushq	%r15
+		movq	%rsi, %r12	# temp for first pointer
+		movq	%rdx, %r13  # temp for second pointer
+		movq	%rsi, %rdi
+		call	swapCase
+
+		pushq	%rax # save first pointer
+		movq	%r12, %rdi
+		call	pstrln
+		movzbl	%al, %r14d	# save first length
+		movq	%r13, %rdi
+		call	swapCase
+
+		pushq	%rax # save second pointer
+		movq	%r13, %rdi
+		call	pstrln
+		movzbl	%al, %r15d	# save second length
+
+		popq	%r13	# second pointer
+		inc		%r13	# skip length byte
+		movl  	%r14d, %esi # first length
+		popq	%rdx	# first pointer
+		inc		%rdx  # skip length byte
+		movq	$0, %rax
+		movq	$func_53, %rdi
+		call 	printf
+
+		movq	$0, %rax
+		movq	$func_53, %rdi
+		movl	%r15d, %esi
+		movq	%r13, %rdx
+		call 	printf
+
+		popq	%r15
+		popq	%r14
+		popq	%r13
+		popq	%r12
+	  	jmp .DONE
+	 
+	.L54:
+		pushq	%rsi	# save first ptr
+		pushq	%rdx	# save second ptr
+		subq	$8, %rsp
+		leaq	4(%rsp), %rsi
+		movq	$f54_sc, %rdi
+		movq	$0, %rax
+		call 	scanf	# scan first int
+		leaq	(%rsp), %rsi
+		movq	$f54_sc, %rdi
+		movq	$0, %rax
+		call 	scanf	# scan second int
+		movb	4(%rsp), %dl
+		movb	(%rsp), %cl
+		movq	8(%rsp), %rsi
+		movq	16(%rsp),%rdi
+		call 	pstrijcmp
+		movl	%eax, %esi
+		movq	$func_54, %rdi
+		movq	$0, %rax
+		call	printf
+		jmp .DONE
+	 
+	.ERR:	 # printing error
+	  movq	 $input_err, %rdi
+	  movq 	 $0, %rax
+	  call 	 printf
+	  jmp 	 .DONE
+	 
+	.DONE:
+	  leave	
+	  ret			
+	
+	
+	
+	
---
+++ pstring.s
@@ -0,0 +1,192 @@
+	.section	.rodata
+	 input_error:  .string "invalid input!\n" # for wrong input
+	
+	.text
+.global pstrln
+.global replaceChar
+.global pstrijcpy
+.global swapCase
+.global pstrijcmp
+
+pstrln:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	movb 	(%rdi), %al	# return val
+	leave
+	ret
+	
+replaceChar:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	movq	%rdi, %rax # saving pointer to string
+	movzbl	(%rdi), %r8d # r8d will have the size of the string
+	movl	$0, %ecx # ecx will be a 'counter'
+	inc		%rdi	# pointer inc
+	dec		%r8d
+	.WHILE:
+	  cmpb	%sil, (%rdi) # check if char == oldChar
+	  je  .TRUE 	# if equal, jmp to true
+	  jmp .FALSE
+	 .TRUE:
+	   movb	%dl, (%rdi) # replace to newChar
+	 .FALSE:
+	   inc	%rdi
+	   inc	%ecx
+	   cmpl	%r8d, %ecx
+	   jle .WHILE
+	leave
+	ret
+	
+pstrijcpy:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	pushq	%r15
+	movq	%rdi, %r15 # saving pointer to string
+	movb	(%rdi), %r8b # saving dst length
+	movb	(%rsi), %r9b # saving src length
+	dec		%r8b
+	dec		%r9b
+	# INPUT CHECK:
+	cmpb	$0, %dl	# i < 0
+	jl .ERR
+	cmpb	$0, %cl	# j < 0
+	jl .ERR
+	cmpb	%r8b, %dl # i > n1
+	ja .ERR
+	cmpb	%r8b, %cl # j > n1
+	ja .ERR
+	cmpb	%r9b, %dl # i > n2
+	ja .ERR
+	cmpb	%r9b, %cl # j > n2
+	ja .ERR
+	jmp .SEC
+	.ERR:
+	  movq 	$input_error, %rdi
+	  movq	$0, %rax
+	  call printf
+		movq	%r15, %rax
+		popq	%r15
+	  leave
+	  ret
+	.SEC:
+		inc	%rdi	# skip length byte
+	  inc	%rsi	# skip length byte
+	  movb	$0, %r10b # 'counter'
+	  cmpb	%dl, %r10b
+	  jl  .WHILE1
+	  jmp .WHILE2
+	 .WHILE1: # increase counter until counter == i
+	   inc	%r10b
+	   inc	%rdi
+	   inc	%rsi
+	   cmpb	%dl, %r10b
+	   jl .WHILE1
+	 # now counter = i
+	 .WHILE2: # switch until counter > j
+	   movb	(%rsi), %r11b
+	   movb	%r11b, (%rdi)
+	   inc	%r10b
+	   inc	%rdi
+	   inc	%rsi
+	   cmpb	%cl, %r10b
+	   jle .WHILE2
+	 movq	 %r15, %rax
+	 popq	 %r15
+	 leave
+	 ret
+	 
+swapCase:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	movq	%rdi, %rax # saving pointer to string
+	movzbl	(%rdi), %r8d # length
+	dec		%r8d
+	inc		%rdi
+	movl	$0, %r9d # 'counter'
+	.while:		# while counter <= length -1
+	  # checking if char is a letter, by ASCII value
+	  cmpb	$122, (%rdi)
+	  ja .pass
+	  cmpb	$65, (%rdi)
+	  jl .pass
+	  cmpb	$91, (%rdi)
+	  jl .capital
+	  cmpb	$96, (%rdi)
+	  ja .small
+	  .capital:		# capital, add 32
+	    addb	$32, (%rdi)
+		  jmp .pass
+	  .small:		# small, dec 32
+	    subb	$32, (%rdi)
+	  .pass:
+	  	inc		%r9d
+			inc		%rdi
+	    cmpl	%r8d, %r9d
+	    jle .while
+	leave
+	ret
+
+pstrijcmp:
+	pushq		%rbp
+	movq		%rsp, %rbp
+	movb	(%rdi), %r8b # saving dst length
+	movb	(%rsi), %r9b # saving src length
+	dec			%r8b
+	dec			%r9b
+	#INPUT CHECK:
+	cmpb	$0, %dl	# i < 0
+	jl .ERROR
+	cmpb	$0, %cl	# j < 0
+	jl .ERROR
+	cmpb	%r8b, %dl # i > n1
+	ja .ERROR
+	cmpb	%r8b, %dl # j > n1
+	ja .ERROR
+	cmpb	%r9b, %dl # i > n2
+	ja .ERROR
+	cmpb	%r9b, %cl # j > n2
+	ja .ERROR
+	jmp .SECS
+	.ERROR:
+	  movq 	$input_error, %rdi
+	  movq	$0, %rax
+	  call   printf
+	  movl	$-2, %eax
+	  leave
+	  ret
+	.SECS:
+		inc	%rdi	# skip length byte
+	  inc	%rsi	# skip length byte
+	  movb	$0, %r10b # 'counter'
+	  cmpb	%dl, %r10b
+	  jl .WHILEA
+	  jmp .WHILEB
+	 .WHILEA:	# increase counter until counter == i
+	   inc	%r10b
+	   inc	%rdi
+	   inc	%rsi
+	   cmpb	%dl, %r10b
+	   jl .WHILEA
+	 # now counter = i
+	 .WHILEB:	# check until counter > j or need to return 1/-1
+	   movb	(%rsi), %r11b
+	   cmpb	%r11b, (%rdi)
+	   jl .SMALLER
+	   ja .Bigger
+	   jmp .PASS
+	   .SMALLER:
+	     movl	$-1, %eax
+	     jmp .DONE
+	   .Bigger:
+	     movl	$1, %eax
+	     jmp .DONE
+	   .PASS: 
+	     inc	%r10b
+	     inc	%rdi
+	     inc	%rsi
+	     cmpb	%cl, %r10b
+	     jle .WHILEB
+	     movl	$0, %eax
+	  .DONE:
+	    leave
+	    ret
